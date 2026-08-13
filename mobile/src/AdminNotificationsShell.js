b4e5076864800acb64ac9bf53abfa298537f4414
vy88mzp5fy-sys <vy88mzp5fy@privaterelay.appleid.com>
2026-08-13 22:03:29 +0100

    Mount admin notification automation controls

--- mobile/src/AdminNotificationsShell.js
+++ mobile/src/AdminNotificationsShell.js
@@ -2,10 +2,12 @@ import React from "react";
 import { View } from "react-native";
 import AdminNotificationsShellCore from "./AdminNotificationsShellCore";
 import AdminWaitingListSessionOverlay from "./AdminWaitingListSessionOverlay";
+import AdminNotificationAutomationOverlay from "./AdminNotificationAutomationOverlay";
 
 export default function AdminNotificationsShellWrapper(props) {
   return <View style={{ flex: 1 }}>
     <AdminNotificationsShellCore {...props} />
     <AdminWaitingListSessionOverlay />
+    <AdminNotificationAutomationOverlay />
   </View>;
 }

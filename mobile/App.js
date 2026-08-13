23f479782c41a6cd49662cec1348a2b913e372bb
vy88mzp5fy-sys <vy88mzp5fy@privaterelay.appleid.com>
2026-08-13 22:07:52 +0100

    Mount client identity and push sync

--- mobile/App.js
+++ mobile/App.js
@@ -1,1 +1,13 @@
-export { default } from "./src/AppShell";
+import React from "react";
+import { View } from "react-native";
+import AppShell from "./src/AppShell";
+import ClientIdentitySync from "./src/ClientIdentitySync";
+import PushDeviceSync from "./src/PushDeviceSync";
+
+export default function App() {
+  return <View style={{ flex: 1 }}>
+    <ClientIdentitySync />
+    <PushDeviceSync />
+    <AppShell />
+  </View>;
+}

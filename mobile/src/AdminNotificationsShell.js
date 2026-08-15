f171c3600c616b413ef993d9f99aed348c0075f5
vy88mzp5fy-sys <vy88mzp5fy@privaterelay.appleid.com>
2026-08-15 03:25:22 +0100

    Remove floating admin launcher buttons

--- mobile/src/AdminNotificationsShell.js
+++ mobile/src/AdminNotificationsShell.js
@@ -1,13 +1,1 @@
-import React from "react";
-import { View } from "react-native";
-import AdminNotificationsShellCore from "./AdminNotificationsShellCore";
-import AdminWaitingListSessionOverlay from "./AdminWaitingListSessionOverlay";
-import AdminNotificationAutomationOverlay from "./AdminNotificationAutomationOverlay";
-
-export default function AdminNotificationsShellWrapper(props) {
-  return <View style={{ flex: 1 }}>
-    <AdminNotificationsShellCore {...props} />
-    <AdminWaitingListSessionOverlay />
-    <AdminNotificationAutomationOverlay />
-  </View>;
-}
+export { default } from "./AdminNotificationsShellCore";

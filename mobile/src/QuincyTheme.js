8efe91d2837be81c392bda34c80426d728dc9f01
vy88mzp5fy-sys <vy88mzp5fy@privaterelay.appleid.com>
2026-08-15 04:03:04 +0100

    Add QuincyFadez luxury design system

---
+++ mobile/src/QuincyTheme.js
@@ -0,0 +1,39 @@
+export const QF = {
+  bg: "#050505",
+  bg2: "#090806",
+  panel: "#0E0D0A",
+  panel2: "#15120C",
+  panel3: "#1B160C",
+  gold: "#D6BD7A",
+  goldSoft: "#F1DDA2",
+  goldDeep: "#B58B35",
+  goldDark: "#6D5423",
+  text: "#FFFDF8",
+  text2: "#EAE5DC",
+  muted: "#AAA39A",
+  muted2: "#756F67",
+  border: "#3B3221",
+  borderSoft: "#242019",
+  green: "#77C98B",
+  greenBg: "#0A1C10",
+  amber: "#E1B85F",
+  amberBg: "#211708",
+  red: "#E39B91",
+  redBg: "#21100D",
+};
+
+export const qfShadow = {
+  shadowColor: "#D6BD7A",
+  shadowOpacity: 0.16,
+  shadowRadius: 18,
+  shadowOffset: { width: 0, height: 8 },
+  elevation: 4,
+};
+
+export const qfCardShadow = {
+  shadowColor: "#000000",
+  shadowOpacity: 0.35,
+  shadowRadius: 14,
+  shadowOffset: { width: 0, height: 8 },
+  elevation: 3,
+};
